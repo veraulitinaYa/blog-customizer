f272d2abf37edce6f2cfe834021ca78394a5cedd
veraulitinaYa <veraulitina@yandex.ru>
2025-09-27 18:57:02 +0300

    Build panel

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,126 @@
+import { useEffect, useRef, useState } from 'react';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import {
+	fontColors,
+	fontFamilyOptions,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+	defaultArticleState,
+} from 'src/constants/articleProps';
+import { Separator } from 'src/ui/separator';
 
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const asideRef = useRef<HTMLElement>(null);
+
+	const [selectedFontFamilyOption, setSelectedFontFamilyOption] = useState(
+		defaultArticleState.fontFamilyOption
+	);
+	const [selectedFontSizeOption, setSelectedFontSizeOption] = useState(
+		defaultArticleState.fontSizeOption
+	);
+	const [selectedFontColor, setSelectedFontColor] = useState(
+		defaultArticleState.fontColor
+	);
+	const [selectedBackgroundColor, setSelectedBackgroundColor] = useState(
+		defaultArticleState.backgroundColor
+	);
+	const [selectedContentWidth, setSelectedContentWidth] = useState(
+		defaultArticleState.contentWidth
+	);
+
+	// Закрытие по клику вне сайдбара
+	useEffect(() => {
+		const handleClickOutside = (event: MouseEvent) => {
+			if (
+				asideRef.current &&
+				!asideRef.current.contains(event.target as Node)
+			) {
+				setIsOpen(false);
+			}
+		};
+
+		if (isOpen) {
+			document.addEventListener('mousedown', handleClickOutside);
+		} else {
+			document.removeEventListener('mousedown', handleClickOutside);
+		}
+
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [isOpen]);
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			{/* повторный клик по стрелке инвертирует isOpen */}
+			<ArrowButton
+				isOpen={isOpen}
+				onClick={(e) => {
+					e.stopPropagation();
+					setIsOpen(!isOpen);
+				}}
+			/>
+			<aside
+				ref={asideRef}
+				className={`${styles.container} ${
+					isOpen ? styles.container_open : ''
+				}`}>
 				<form className={styles.form}>
+					{/* 1. Select */}
+					<Select
+						title='Шрифт'
+						options={fontFamilyOptions}
+						selected={selectedFontFamilyOption}
+						onChange={setSelectedFontFamilyOption}
+						placeholder='Выберите опцию'
+					/>
+
+					{/* 2. Radio group */}
+					<RadioGroup
+						title='Группа радио'
+						name='radio-group'
+						options={fontSizeOptions}
+						selected={selectedFontSizeOption}
+						onChange={setSelectedFontSizeOption}
+					/>
+
+					{/* 3. Select */}
+					<Select
+						title='Цвет шрифта'
+						options={fontColors}
+						selected={selectedFontColor}
+						onChange={setSelectedFontColor}
+						placeholder='Выберите опцию'
+					/>
+
+					{/* 4. Separator */}
+					<Separator />
+
+					{/* 5. Select */}
+					<Select
+						title='Третий селект'
+						options={backgroundColors}
+						selected={selectedBackgroundColor}
+						onChange={setSelectedBackgroundColor}
+						placeholder='Выберите опцию'
+					/>
+
+					{/* 6. Select */}
+					<Select
+						title='Четвертый селект'
+						options={contentWidthArr}
+						selected={selectedContentWidth}
+						onChange={setSelectedContentWidth}
+						placeholder='Выберите опцию'
+					/>
+
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
--- src/ui/arrow-button/ArrowButton.tsx
+++ src/ui/arrow-button/ArrowButton.tsx
@@ -4,7 +4,7 @@ import styles from './ArrowButton.module.scss';
 import clsx from 'clsx';
 
 /** Функция для обработки открытия/закрытия формы */
-export type OnClick = () => void;
+export type OnClick = (e: React.MouseEvent<HTMLDivElement>) => void;
 
 type ArrowButtonProps = {
 	isOpen: boolean;
@@ -13,7 +13,6 @@ type ArrowButtonProps = {
 
 export const ArrowButton = ({ isOpen, onClick }: ArrowButtonProps) => {
 	return (
-		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
